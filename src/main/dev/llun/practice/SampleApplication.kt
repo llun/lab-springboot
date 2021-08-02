9111f030e2a0be123a25d2d949c9c7afc232af6f
Maythee Anegboonlap <null@llun.me>
2021-08-02 15:07:30 +0800

    Init

---
+++ src/main/dev/llun/practice/SampleApplication.kt
@@ -0,0 +1,11 @@
+package dev.llun.practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleApplication>(*args)
+}
