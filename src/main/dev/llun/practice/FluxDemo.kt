3c7d7b0ce09bdca0ff3fd35d58dbd5604ca8d273
Maythee Anegboonlap <null@llun.me>
2021-11-09 19:58:26 +0800

    Play with api path

--- src/main/dev/llun/practice/FluxDemo.kt
+++ src/main/dev/llun/practice/FluxDemo.kt
@@ -1,4 +1,19 @@
 package dev.llun.practice
 
-class FluxDemo {
+import reactor.core.publisher.Flux
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
+
+fun main(args: Array<String>) {
+    var beers : List<String> = listOf("Heineken", "Guinness", "Tiger", "Sapporo", "Asahi", "Kronenbourg", "Carlsberg")
+    var flux:Flux<String> = Flux.fromIterable(beers)
+
+    flux
+        .buffer(3)
+        .flatMap { beers ->
+            beers.slice(0..if (beers.size > 2) 1 else 0).toMono()
+        }
+        .reduce{ acc, s -> s?.let { acc?.plus(it) } }
+        .map { println(it) }
+        .subscribe()
 }
--- src/main/dev/llun/practice/controllers/BlogController.kt
+++ src/main/dev/llun/practice/controllers/BlogController.kt
@@ -3,14 +3,21 @@ package dev.llun.practice.controllers
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 
 @Controller
+@RequestMapping("/api")
 class BlogController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     fun hello(model: Model):String {
         return "hello"
+    }
+
+    @GetMapping("/something/test")
+    fun something(model: Model):String {
+        return "something"
     }
 
 }
