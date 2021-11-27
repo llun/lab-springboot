91c59724b092b81e46ff20c0d3d3077fa5bbee64
Maythee Anegboonlap <null@llun.me>
2021-11-27 11:28:17 +0800

    Add Java playground

--- src/main/dev/llun/practice/PatternPlayground.kt
+++ src/main/dev/llun/practice/PatternPlayground.kt
@@ -3,9 +3,11 @@ package dev.llun.practice
 import org.springframework.http.server.PathContainer
 import org.springframework.web.util.pattern.PathPatternParser
 
-fun main(args: Array<String>) {
+fun main() {
     val patternMatcher = PathPatternParser().parse("/api/resource")
     println(patternMatcher.matches(PathContainer.parsePath("/api;something-else/resource")))
+    println(patternMatcher.matches(PathContainer.parsePath("/api,something-else/resource")))
+    println(patternMatcher.matches(PathContainer.parsePath("/api!something-else/resource")))
     println(patternMatcher.matches(PathContainer.parsePath("/api/resource")))
     println(patternMatcher.matches(PathContainer.parsePath("/api1/resource")))
 }
